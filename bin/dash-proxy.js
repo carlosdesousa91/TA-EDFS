f47f2293e4ab316079d1ef370144860b709dcbce
carlosdesousa91 <31635606+carlosdesousa91@users.noreply.github.com>
2021-01-07 20:03:07 -0300

    Update dash-proxy.js
    https

--- bin/dash-proxy.js
+++ bin/dash-proxy.js
@@ -1,24 +1,25 @@
-// __author__ = 'Michael Uschmann / MuS'
-// __date__ = 'Copyright $Oct 25, 2018 11:00:00 AM$'
-// __version__ = '0.1.1'
+// __author__ = 'Carlos araújo de sousa carlos.desousa91@outlook.com'
+// __date__ = 'Copyright $Oct 07, 2021 19:00:00 PM$'
+// __version__ = '0.1.1.1'
 
-// get config arguments
+var fs = require('fs');
+var httpProxy = require('http-proxy');
+var https = require('https');
+
+//get config arguments
 var remote_user = process.argv[2],
-    locale = 'en-GB',
-    proxy_port = process.argv[3],
-    connect_from = process.argv[4],
-    splunk_port = '8000';
+locale = 'en-US',
+proxy_port = process.argv[3],
+connect_from = process.argv[4],
+splunk_port = '8000';
 
-// fixed variables - HaHA!
-var myPath = '/opt/splunk/etc/apps/TA-EDFS/bin/'
-    http = require('http'),
-    httpProxy = require('http-proxy');
-    // https = require('https'),
-    // fs = require('fs');
-
-// Create a proxy server with custom application logic
-var proxy = httpProxy.createProxyServer({
-    target: 'http://127.0.0.1:' + splunk_port
+var proxy = httpProxy.createServer({
+  ssl:{
+    key: fs.readFileSync('/opt/splunk/etc/auth/mycerts/mykey.key',  'utf8'),
+    cert: fs.readFileSync('/opt/splunk/etc/auth/mycerts/mycert.pem',  'utf8')
+  },
+  target: 'https://127.0.0.1:' + splunk_port,
+  secure: false
 });
 
 // Modify the header to allow the user to login
@@ -27,32 +28,5 @@ proxy.on('proxyReq', function(proxyReq, req, res, options) {
   proxyReq.setHeader('REMOTE_USER', remote_user);
 });
 
-var server = http.createServer(function(req, res) {
-  // check if the client IP is allowed to connect
-  var epoch = (new Date).getTime();
-  var client_ip = req.connection.remoteAddress.replace(/^[:f]+/, '');
-  if (!(client_ip == connect_from )) {
-     console.log("_time=\"" + epoch + "\" clientIp=\"" + req.connection.remoteAddress.replace(/^[:f]+/, '') + "\" proxy=\"" + req.headers.host + "\" message=\"Emergency, paging Dr. Beat! Someone is connecting from the wrong IP!!!\" url=\"" + req.url + "\"");
-     return;
-  }
+proxy.listen(8080);
 
-  // check if we have a search request in the url
-  var search_check = req.url.match(/(\/search\/jobs)/);
-
-  // check for HTTP POST that is NOT a search, and exit!
-  var epoch = (new Date).getTime();
-  if (req.method == 'POST' && search_check == null ) {
-     console.log("_time=\"" + epoch + "\" user=\"" + remote_user + "\" clientIp=\"" + req.connection.remoteAddress.replace(/^[:f]+/, '') + "\" proxy=\"" + req.headers.host + "\" message=\"Emergency, paging Dr. Beat! Someone is using a POST!!!\" url=\"" + req.url + "\"");
-     return;
-  }
-
-  // allow connection and proxy it to Splunk
-  var epoch = (new Date).getTime();
-  console.log(remote_user);
-  console.log("_time=\"" + epoch + "\" user=\"" + remote_user + "\" clientIp=\"" + req.connection.remoteAddress.replace(/^[:f]+/, '') + "\" proxy=\"" + req.headers.host + "\" url=\"" + req.url + "\"" );
-  proxy.web(req, res);
-});
-
-var start_epoch = (new Date).getTime();
-console.log("_time=" + start_epoch + " message=\"Starting Dashboard proxy on port " + proxy_port + "\"")
-server.listen(proxy_port);
